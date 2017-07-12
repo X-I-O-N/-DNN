4124d5b70bb99efe0b6a58a0ed3c3123d0f4a9e0
X-I-O-N <teerth9000@gmail.com>
2017-07-11 22:53:18 -0700

    Update DNNbinary.py

--- DNNbinary.py
+++ DNNbinary.py
@@ -76,7 +76,7 @@ def wider_model():
 	model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 	return model
 
-modelname = "DNNGPVOTE"
+modelname = "DTGBLEND"
 if modelname == "keras":
 	estimators = []
 	estimators.append(('standardize', StandardScaler()))
@@ -103,6 +103,18 @@ if modelname == "DNNGPBLEND":
 	lr = lm.LogisticRegression()
  	#gbc = sklearn.ensemble.GradientBoostingClassifier()
 	models = [StackingRegressor(regressors=[DNN,est_gp], meta_regressor=lr)]
+
+if modelname == "DTGPBLEND":
+	clf1 = sklearn.tree.DecisionTreeRegressor(max_depth=4)
+	est_gp = SymbolicRegressor(population_size=5000,
+                           generations=20, stopping_criteria=0.01,
+                           p_crossover=0.7, p_subtree_mutation=0.1,
+                           p_hoist_mutation=0.05, p_point_mutation=0.1,
+                           max_samples=0.9, verbose=1,
+                           parsimony_coefficient=0.01, random_state=0)
+	lr = lm.LogisticRegression()
+ 	#gbc = sklearn.ensemble.GradientBoostingClassifier()
+	models = [StackingRegressor(regressors=[clf1,est_gp], meta_regressor=lr)]
 
 if modelname == "DNNGPVOTE":
 	estimators = []
