6e2af5b584e8ddd1e2e6bcf60c47f7617d704c92
X-I-O-N <teerth9000@gmail.com>
2017-07-11 22:02:26 -0700

    Update DNNbinary.py

--- DNNbinary.py
+++ DNNbinary.py
@@ -23,13 +23,14 @@ numpy.random.seed(seed)
 dataframe = pandas.read_csv("L-all-blip-else-trainingset.csv", header=None)
 dataset = dataframe.values
 # split into input (X) and output (Y) variables
-X = dataset[:,0:1288].astype(float)
+#X = dataset[:,0:1288].astype(float)
+X = dataset[:,0:1288]
 Y = dataset[:,1288]
 
 # encode class values as integers
-encoder = LabelEncoder()
-encoder.fit(Y)
-encoded_Y = encoder.transform(Y)
+#encoder = LabelEncoder()
+#encoder.fit(Y)
+#encoded_Y = encoder.transform(Y)
 
 # baseline model
 def create_baseline():
@@ -63,6 +64,16 @@ def create_smaller():
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
 
+# define wider model
+def wider_model():
+	# create model
+	model = Sequential()
+	model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
+	model.add(Dense(1, kernel_initializer='normal'))
+	# Compile model
+	model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+	return model
+
 modelname = "DNNGPBLEND"
 if modelname == "keras":
 	estimators = []
@@ -72,7 +83,7 @@ if modelname == "keras":
 if modelname == "DNNGPBLEND":
 	estimators = []
 	estimators.append(('standardize', StandardScaler()))
-	estimators.append(('mlp', KerasRegressor(build_fn=create_smaller, epochs=100, batch_size=5, verbose=0)))
+	estimators.append(('mlp', KerasRegressor(build_fn=wider_model, epochs=100, batch_size=5, verbose=0)))
 	DNN = Pipeline(estimators)
 	est_gp = SymbolicRegressor(population_size=5000,
                            generations=20, stopping_criteria=0.01,
@@ -85,5 +96,7 @@ if modelname == "DNNGPBLEND":
 	models = [StackingRegressor(regressors=[DNN,est_gp], meta_regressor=lr)]
 
 kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=seed)
-results = cross_val_score(models, X, encoded_Y, cv=kfold)
+#results = cross_val_score(models, X, encoded_Y, cv=kfold)
+results = cross_val_score(models, X, Y, cv=kfold)
 print("Accuracy: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+print("Wider: %.2f (%.2f) MSE" % (results.mean(), results.std()))
