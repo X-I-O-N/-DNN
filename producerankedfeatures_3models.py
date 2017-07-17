17af2762ac8ecfb40ac4bc6246aa9b64fd4ed5b5
X-I-O-N <teerth9000@gmail.com>
2017-07-16 21:12:47 -0700

    Update producerankedfeatures_3models.py

--- producerankedfeatures_3models.py
+++ producerankedfeatures_3models.py
@@ -15,7 +15,7 @@ import pandas
 #from keras.wrappers.scikit_learn import KerasRegressor
 #from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import StratifiedKFold
+from sklearn.cross_validation import StratifiedKFold
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
 from gplearn.genetic import SymbolicRegressor
@@ -23,51 +23,80 @@ from mlxtend.classifier import StackingClassifier
 from mlxtend.regressor import StackingRegressor
 import sklearn.linear_model as lm
 from xgboost import XGBClassifier
+from sklearn.feature_selection import RFECV
 #from xgboost import plot_importance
 # fix random seed for reproducibility
 seed = 7
 numpy.random.seed(seed)
 
 # load dataset
-dataframe = pandas.read_csv("L-aux-endxmag-else-trainingset.csv", header=None)
+dataframe = pandas.read_csv("L-all-blip-else-trainingset.csv", header=None)
 dataset = dataframe.values
 # split into input (X) and output (Y) variables
 #X = dataset[:,0:4050].astype(float)
-X = dataset[:,0:4050]
-y = dataset[:,4050]
+X = dataset[:,0:1288]
+y = dataset[:,1288]
 
 # encode class values as integers
 #encoder = LabelEncoder()
 #encoder.fit(Y)
 #y = encoder.transform(Y)
 
-forest = ExtraTreesClassifier(n_estimators=250,
-                              random_state=0)
-xgb = XGBClassifier()
-xgb.fit(X, y)
+#forest = ExtraTreesClassifier(n_estimators=250,
+                             # random_state=0)
+#xgb = XGBClassifier()
+#xgb.fit(X, y)
 #plot_importance(xgb)
-DTC = sklearn.tree.DecisionTreeClassifier()
-DTC.fit(X, y)
+#DTC = sklearn.tree.DecisionTreeClassifier()
+#DTC.fit(X, y)
 
 
 
-forest.fit(X, y)
+#forest.fit(X, y)
 #print cross_val_score(forest, X, y, cv=5)
 
-importances1 = forest.feature_importances_
-importances2= xgb.feature_importances_
-importances3 = DTC.feature_importances_
+#importances1 = forest.feature_importances_
+#importances2= xgb.feature_importances_
+#importances3 = DTC.feature_importances_
 
-importances = ((importances1 + importances2 + importances3)/3)
-std = np.std([tree.feature_importances_ for tree in forest.estimators_],
-             axis=0)
-indices = np.argsort(importances)[::-1]
+#importances = ((importances1 + importances2 + importances3)/3)
+#std = np.std([tree.feature_importances_ for tree in forest.estimators_],
+           #  axis=0)
+#indices = np.argsort(importances)[::-1]
 
 # Print the feature ranking
-print("Feature ranking:")
+#print("Feature ranking:")
 
-for f in range(X.shape[1]):
-    print("%d. %d %f" % (f + 1, (indices[f])+1, importances[indices[f]]))
+#for f in range(X.shape[1]):
+    #print("%d. %d %f" % (f + 1, (indices[f])+1, importances[indices[f]]))
+
+classifier = XGBClassifier(
+    objective='binary:logistic', 
+    n_estimators=200, 
+    learning_rate=0.08, 
+    max_depth=5, 
+    nthread=4,
+    subsample=0.9,
+#    colsample_bytree=0.8,
+    reg_lambda=6,
+    reg_alpha=5,
+    seed=1301,
+    silent=True
+)
+selector = RFECV(estimator=classifier, step=3, 
+   cv=5)
+selector.fit(X, y)
+
+print('The optimal number of features is {}'.format(selector.n_features_))
+features = [f for f,s in zip(X_train.columns, selector.support_) if s]
+print('The selected features are:')
+print ('{}'.format(features))
+
+#plt.figure()
+#plt.xlabel("Number of features selected")
+#plt.ylabel("Cross validation score (roc auc)")
+#plt.plot(range(1, len(selector.grid_scores_) + 1), selector.grid_scores_)
+#plt.savefig('feature_auc_nselected.png', bbox_inches='tight', pad_inches=1)
 
 # Plot the feature importances of the forest
 #plt.figure()
